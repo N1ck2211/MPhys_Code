ad74aed1d1117be516ef3d8e8e5ca3b812aabfbc
Nick Taylor <100151742+N1ck2211@users.noreply.github.com>
2024-02-23 10:42:40 +0000

    bug fixing

--- histmakers/all_score_histmaker.py
+++ histmakers/all_score_histmaker.py
@@ -1,5 +1,5 @@
 # list of processes (mandatory)
-processList = {"wzp6_ee_Hgg_ecm125": {"fraction": 1},}
+processList = {"wzp6_ee_Hgg_ecm125": {"fraction": 1}, "wzp6_ee_qq_ecm125": {"fraction": 1}}
 
 # Production tag when running over EDM4Hep centrally produced events, this points to the yaml files for getting sample statistics (mandatory)
 # prodTag = "FCCee/winter2023/IDEA/"
@@ -17,7 +17,7 @@ outputDir = "./full_clustering_output/histmaker"
 nCPUS = -1
 
 # scale the histograms with the cross-section and integrated luminosity
-doScale = False
+doScale = True
 intLumi = 10000000  # 10 /ab
 
 score_to_cut = 0.0
@@ -27,23 +27,24 @@ score_to_cut = 0.0
 # define some binning for various histograms
 bins_p_mu = (2000, 0, 200)  # 100 MeV bins
 bins_m_ll = (2000, 0, 200)  # 100 MeV bins
-bins_p = (2000, -100, 100)
+bins_p = (700, 0, 70)
 bins_p_ll = (2000, 0, 200)  # 100 MeV bins
 bins_recoil = (200000, 0, 200)  # 1 MeV bins
 bins_cosThetaMiss = (10000, 0, 1)
 
 bins_m_jj = (100, 50, 150)  # 1 GeV bins
-bins_score = (50, 0, 2.0)  #
+bins_score = (50, 0, 2.0)  
+bins_onej_score = (50, 0, 1.0)  
 bins_n  = (50, 0, 20000000)
 
 bins_theta = (500, -5, 5)
-bins_eta = (600, -3, 3)
+bins_eta = (1200, -6, 6)
 bins_phi = (500, -1, 1)
 
 bins_count = (50, 0, 50)
 bins_charge = (9, -4.5, 4.5)
 bins_iso = (500, 0, 5)
-bins_e = (600, 0, 60)
+bins_e = (900, 0, 90)
 
 # build_graph function that contains the analysis logic, cuts and histograms (mandatory)
 def build_graph(df, dataset):
@@ -52,8 +53,8 @@ def build_graph(df, dataset):
     df = df.Define("weight", "1.0")
     weightsum = df.Sum("weight")
 
-    df = df.Define('Jet_One', 'recojet_isB[0]')
-    df = df.Define('Jet_Two', 'recojet_isB[1]')
+    df = df.Define('Jet_One', 'recojet_isG[0]')
+    df = df.Define('Jet_Two', 'recojet_isG[1]')
 
     df = df.Define("scoresum_B", "recojet_isB[0] + recojet_isB[1]")
     df = df.Define("scoresum_Q", "recojet_isQ[0] + recojet_isQ[1]")
@@ -62,8 +63,8 @@ def build_graph(df, dataset):
     df = df.Define("scoresum_G", "recojet_isG[0] + recojet_isG[1]")
 
     # Create Histograms for individual jets
-    results.append(df.Histo1D(('Jet_One', '', *bins_score), 'Jet_One'))
-    results.append(df.Histo1D(('Jet_Two', '', *bins_score), 'Jet_Two'))
+    results.append(df.Histo1D(('Jet_One', '', *bins_onej_score), 'Jet_One'))
+    results.append(df.Histo1D(('Jet_Two', '', *bins_onej_score), 'Jet_Two'))
 
     results.append(df.Histo1D(("scoresum_B", "", *bins_score), "scoresum_B"))
     results.append(df.Histo1D(("scoresum_Q", "", *bins_score), "scoresum_Q"))
@@ -78,8 +79,7 @@ def build_graph(df, dataset):
     results.append(df.Histo1D(("jj_theta", "", *bins_theta), "jj_theta"))
     results.append(df.Histo1D(("jj_phi", "", *bins_phi), "jj_phi"))
     results.append(df.Histo1D(("jj_m", "", *bins_m_jj), "jj_m"))
-    results.append(df.Histo1D(("jj_eta", "", *bins_m_jj), "jj_eta"))
-    # results.append(df.Histo1D(("jj_q", "", *bins_charge), "jj_q"))
+    results.append(df.Histo1D(("jj_eta", "", *bins_eta), "jj_eta"))
 
     results.append(df.Histo1D(("jj_e", "", *bins_e), "jj_e"))
     results.append(df.Histo1D(("jj_pt", "", *bins_p), "jj_pt"))
